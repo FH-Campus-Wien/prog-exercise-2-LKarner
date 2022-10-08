ccfe709ae85bbf04284b76d68d910a19728f5405
LKarner <lukas.karner1@gmail.com>
2022-10-08 15:57:04 +0200

    Exercise 1 solved.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,13 +1,48 @@
 package at.ac.fhcampuswien;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
 
     //todo Task 1
-    public void largestNumber(){
+    public void largestNumber() {
         // input your solution here
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.US); //Erlaubt Dezimaleingaben mit "." statt "," (US-Standard).
+        // Source: https://stackoverflow.com/questions/49478968/input-double-with-dot-instead-of-comma
+
+        double largestNumber = 0;
+        double numbers; //Variable zur Speicherung der Benutzereingaben.
+        int counter = 1; //Speicherung fortlaufender Nummerierung.
+
+        System.out.print("Number " + counter + ": ");
+        counter ++; //Zählt counter +1, da bei Ausführung der Schleife bereits Eingabe für Nummer 1 erforderlich.
+
+        numbers = scan.nextDouble();
+
+        if (numbers <= 0) { //Erste Prüfung, ob Zahl > 0 eingegeben.
+            System.out.println("No number entered.");
+            return;
+        }
+
+        while (true) {
+
+            if (numbers > largestNumber) {
+                largestNumber = numbers; //Speichert größte Zahl
+            }
+            if (numbers <= 0) {
+                System.out.println("The largest number is " + String.format(Locale.US, "%.2f", largestNumber));
+                //Gibt die größte Zahl aus. String.format -->.2f für Dezimalzahl mit 2 Nachkommerstellen.
+                return; //Ausbruch aus endlos-Schleife
+            }
+            System.out.print("Number " + counter + ": ");
+            counter++; //Erhöht Counter +1 in der Schleife
+            numbers = scan.nextDouble(); //Liest nächste Zahl ein.
+        }
+
     }
+
 
     //todo Task 2
     public void stairs(){
